f4f82fc1a86865a4b8dca67abe61cc2fe1aea40c
Maksym Shyiko <maxymshyjko@gmail.com>
2023-12-08 10:01:12 +0100

    Implemented routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home/all', pathMatch: 'full' },
+  // { path: 'home', component: HomeComponent },
+  // { path: 'characters', component: WAlcoholComponent },
+  // { path: 'episodes', component: WOAlcoholComponent },
+  // { path: 'locations', component: NewCocktailComponent },
+  // { path: 'about', component: NameFilterComponent },
+  // { path: 'character/:id', component: ObservablePageComponent},
+  // { path: 'location/:id', component: CocktailDetailComponent},
+  // { path: 'episode/:id', component: PasswordComponent},
+];  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
