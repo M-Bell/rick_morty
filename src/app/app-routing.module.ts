104fe0a8cd5afb6c4878cdfa62f6ab9e2a91ea47
Maksym Shyiko <maxymshyjko@gmail.com>
2023-12-08 10:09:29 +0100

    Initialised components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { EpisodesListComponent } from './episodes-list/episodes-list.component';
+import { LocationsListComponent } from './locations-list/locations-list.component';
+import { AboutComponent } from './about/about.component';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
+import { LocationDetailComponent } from './location-detail/location-detail.component';
+import { EpisodeDetailComponent } from './episode-detail/episode-detail.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home/all', pathMatch: 'full' },
-  // { path: 'home', component: HomeComponent },
-  // { path: 'characters', component: WAlcoholComponent },
-  // { path: 'episodes', component: WOAlcoholComponent },
-  // { path: 'locations', component: NewCocktailComponent },
-  // { path: 'about', component: NameFilterComponent },
-  // { path: 'character/:id', component: ObservablePageComponent},
-  // { path: 'location/:id', component: CocktailDetailComponent},
-  // { path: 'episode/:id', component: PasswordComponent},
-];  
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'characters', component: CharacterListComponent },
+  { path: 'episodes', component: EpisodesListComponent },
+  { path: 'locations', component: LocationsListComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'character/:id', component: CharacterDetailComponent},
+  { path: 'location/:id', component: LocationDetailComponent},
+  { path: 'episode/:id', component: EpisodeDetailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { EpisodesListComponent } from './episodes-list/episodes-list.component';
+import { LocationsListComponent } from './locations-list/locations-list.component';
+import { LocationDetailComponent } from './location-detail/location-detail.component';
+import { EpisodeDetailComponent } from './episode-detail/episode-detail.component';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CharacterListComponent,
+    EpisodesListComponent,
+    LocationsListComponent,
+    LocationDetailComponent,
+    EpisodeDetailComponent,
+    CharacterDetailComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
