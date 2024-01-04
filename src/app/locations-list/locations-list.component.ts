580d2d0cff00bd9620f43ba1205f459ff520ae19
Maksym Shyiko <maxymshyjko@gmail.com>
2024-01-04 13:27:59 +0100

    Added search bar to locations list

--- src/app/locations-list/locations-list.component.ts
+++ src/app/locations-list/locations-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DataService } from '../data.service';
 import { Location } from '../location';
+import { FormControl, UntypedFormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-locations-list',
@@ -9,12 +10,27 @@ import { Location } from '../location';
 })
 export class LocationsListComponent {
   locations: Location[]
+  formGroup: UntypedFormGroup
+  searchControl: FormControl<string>
+  empty_list=false
 
   constructor(private dataService: DataService){
     this.locations = new Array<Location>
+    this.searchControl = new FormControl('', { nonNullable: true })
+    this.formGroup = new UntypedFormGroup({
+        search: this.searchControl
+    })
   }
 
   ngOnInit(): void {
     this.dataService.getAllLocations().subscribe(val => this.locations = val)
+    this.searchControl.valueChanges.subscribe(val => {
+      this.locations=[];
+      if(val.length>0){
+        this.dataService.getAllLocations(val).subscribe(val => this.locations = val)
+      } else {
+        this.dataService.getAllLocations().subscribe(val => this.locations = val)
+      };
+    });
   }
 }
